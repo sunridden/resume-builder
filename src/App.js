33c4df7555b7c2fe32cd33562091ea78cd07c56f
sunridden <kevinszeto9449@gmail.com>
2023-04-24 18:19:07 -0700

    Remove boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Resume Builder App
-        </p>
-      </header>
+import React, { Component } from "react";
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+    <div>
+    <form>
+      <label>Personal Information
+        <input type='text' id='information' name='information' placeholder='Enter your title, first- and last name'></input>
+        <input type='text' id='information' name='information' placeholder='Enter job title'></input>
+        <input type='text' id='information' name='information' placeholder='Enter email'></input>
+        <input type='text' id='information' name='information' placeholder='Enter phone number'></input>
+        <input type='text' id='information' name='information' placeholder='City, Country'></input>
+        <button>Hit me!</button>
+      </label>
+      <label>Experience
+        
+      </label>
+    </form>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
